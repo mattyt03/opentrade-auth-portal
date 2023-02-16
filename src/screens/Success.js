95532de077b5561bfef0c62899df63821d974f4b
Matthew Tolea <matthewtolea@gmail.com>
2023-02-16 04:19:58 -0600

    added checkmark animation to success page

--- src/screens/Success.js
+++ src/screens/Success.js
@@ -1,9 +1,13 @@
 import Card from '../components/Card';
+import { Checkmark } from 'react-checkmark';
 
 const Success = () => {
     return (
         <Card>
             <div className='success'>
+                {/* putting the checkmark in a div fucks things up */}
+                {/* make the color a global var */}
+                <Checkmark size='xLarge' color='#6772e5'/>
                 <h1 className='success__h1'>Success!</h1>
                 <p className='success-message'>Your account has been successfully linked this application.</p>
             </div>
